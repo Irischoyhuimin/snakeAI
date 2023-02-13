7cae1a83b217338e4c6885b01180a04921926634
Irischoyhuimin <irischm@outlook.com>
2023-02-13 07:52:11 -0500

    Updates

---
+++ precache-manifest.bf11f7f8ef2860e76db35afda64ddb60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb63e06eefc50eb824d6f79c00a10c19",
+    "url": "/snakeAI/index.html"
+  },
+  {
+    "revision": "9ab4ca5f7b2da1b30a30",
+    "url": "/snakeAI/static/css/2.765c86df.chunk.css"
+  },
+  {
+    "revision": "fd5aef17399542982d01",
+    "url": "/snakeAI/static/css/main.d1c41df7.chunk.css"
+  },
+  {
+    "revision": "9ab4ca5f7b2da1b30a30",
+    "url": "/snakeAI/static/js/2.f8ea9455.chunk.js"
+  },
+  {
+    "revision": "3c7949eb956b073fdfb064045c45a0dc",
+    "url": "/snakeAI/static/js/2.f8ea9455.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5aef17399542982d01",
+    "url": "/snakeAI/static/js/main.8e94a9ff.chunk.js"
+  },
+  {
+    "revision": "2686a32751f7ee6876bd",
+    "url": "/snakeAI/static/js/runtime-main.925a331e.js"
+  }
+]);
